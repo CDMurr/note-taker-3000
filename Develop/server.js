4b985ec550ba2af6d244cd41d677e9bed18033f1
Cody Murray <cdillon.murray@yahoo.com>
2022-04-21 14:02:08 -0500

    connected styling and js to notes.html by updating file path

--- Develop/server.js
+++ Develop/server.js
@@ -5,10 +5,15 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
+});
+
+// i thought this would allow to Get notes.html?
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './notes'));
 });
 
 app.listen(PORT, () => {
