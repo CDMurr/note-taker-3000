916f2d45f3e910545f4b64e486d05eb022a9baa9
Cody Murray <cdillon.murray@yahoo.com>
2022-04-21 13:35:46 -0500

    got html to pull up on local server

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
+const path = require("path");
 const app = express();
 
-app.listen(3001, () => {
-    console.log('API server now port 3001!');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
