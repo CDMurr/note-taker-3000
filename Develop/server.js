b92874448a29b8f39650a1a2add61c1d46abf76a
Cody Murray <cdillon.murray@yahoo.com>
2022-04-21 20:46:13 -0500

    updated file paths

--- Develop/server.js
+++ Develop/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const path = require("path");
+const fs = require("fs");
 const app = express();
+
+const notesData = require('./db/db.json');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './index.html'));
 });
 
-// i thought this would allow to Get notes.html?
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './notes'));
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+})
+
+// i thought this would allow to Get notes.html?. 
+// updated with note class 
+app.get('/api/notes', (req, res) => res.json(notesData));
+app.post('/api/notes', (req, res) => {
+    req.body.id = notesData.length
+    const newNote = req.body 
+    notesData.push(newNote);
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notesData))
+    res.json(newNote)
 });
 
 app.listen(PORT, () => {
